0fca8b2d93b5900d5545478afe368ad0ece5e072
sktbrd <sktbrd.eth@gmail.com>
2025-04-25 10:55:35 -0300

    Refactor ImageUploader component for improved readability and consistency

--- components/homepage/ImageUploader.tsx
+++ components/homepage/ImageUploader.tsx
@@ -1,41 +1,42 @@
-import React, { useRef } from 'react';
-import { Box, Image, IconButton, Wrap, Input, Button } from '@chakra-ui/react';
-import { CloseIcon } from '@chakra-ui/icons';
+import React, { useRef } from "react";
+import { Box, Input } from "@chakra-ui/react";
 
 interface ImageUploaderProps {
-    onUpload: (files: File[]) => void;
-    onRemove: (index: number) => void;
-    images: File[];
+  onUpload: (files: File[]) => void;
+  onRemove: (index: number) => void;
+  images: File[];
 }
 
-const ImageUploader: React.FC<ImageUploaderProps> = ({ onUpload, onRemove, images }) => {
-    const inputRef = useRef<HTMLInputElement>(null);
+const ImageUploader: React.FC<ImageUploaderProps> = ({
+  onUpload,
+  onRemove,
+  images,
+}) => {
+  const inputRef = useRef<HTMLInputElement>(null);
 
-    const handleImageUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const files = Array.from(event.target.files || []);
-        onUpload(files);
-    };
+  const handleImageUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const files = Array.from(event.target.files || []);
+    onUpload(files);
+  };
 
-    const triggerFileInput = () => {
-        if (inputRef.current) {
-            inputRef.current.click();
-        }
-    };
+  const triggerFileInput = () => {
+    if (inputRef.current) {
+      inputRef.current.click();
+    }
+  };
 
-    return (
-        <Box onClick={triggerFileInput}>
-
-            <Input
-                ref={inputRef}
-                type="file"
-                multiple
-                accept="image/*"
-                onChange={handleImageUpload}
-                hidden
-            />
-
-        </Box>
-    );
+  return (
+    <Box onClick={triggerFileInput}>
+      <Input
+        ref={inputRef}
+        type="file"
+        multiple
+        accept="image/*"
+        onChange={handleImageUpload}
+        hidden
+      />
+    </Box>
+  );
 };
 
 export default ImageUploader;
