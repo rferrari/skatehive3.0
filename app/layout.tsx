e2e7058784cc73eefd04eae1d950770d010871a6
sktbrd <sktbrd.eth@gmail.com>
2025-06-10 14:15:20 -0300

    feat: Initialize Frame SDK in layout and add OpenGraph images

--- app/layout.tsx
+++ app/layout.tsx
@@ -65,8 +65,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`${vt323.variable}`}>
-                <InitFrameSDK />
-
+      <InitFrameSDK />
       <body>
         <RootLayoutClient>{children}</RootLayoutClient>
       </body>
