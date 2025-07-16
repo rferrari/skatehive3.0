1e75c031f912933a3ed0fb649c5a4a84c3436da2
webgnar <zgeogoulet@gmail.com>
2025-07-15 19:48:29 -0500

    extract GPS from photo in spotSnapComposer

--- declarations.d.ts
+++ declarations.d.ts
@@ -3,4 +3,8 @@ declare module '@aioha/aioha';
 
 interface Window {
   hive_keychain?: any;
+}
+
+declare module 'exifr' {
+  export function gps(file: File): Promise<{ latitude?: number; longitude?: number } | undefined>;
 } 
