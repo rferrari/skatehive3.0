11c8acd403ef2e93ebb20f52adde5cfe6928d2f8
sktbrd <sktbrd.eth@gmail.com>
2025-07-28 12:20:41 -0300

    style: format code for consistency in ProfileCoverImage component

--- components/profile/ProfileCoverImage.tsx
+++ components/profile/ProfileCoverImage.tsx
@@ -3,31 +3,34 @@ import React from "react";
 import { Box, Image } from "@chakra-ui/react";
 
 interface ProfileCoverImageProps {
-    coverImage: string;
-    username: string;
+  coverImage: string;
+  username: string;
 }
 
-export default function ProfileCoverImage({ coverImage, username }: ProfileCoverImageProps) {
-    return (
-        <Box
-            position="relative"
-            w={{ base: "100vw", md: "100%" }}
-            maxW={{ base: "100vw", md: "container.lg" }}
-            mx={{ base: "unset", md: "auto" }}
-            overflow="hidden"
-            height={{ base: "120px", md: "200px" }}
-            p={0}
-            m={0}
-            mt={4}
-        >
-            <Image
-                src={coverImage}
-                alt={`${username} cover`}
-                w={{ base: "100vw", md: "100%" }}
-                h={{ base: "120px", md: "200px" }}
-                objectFit="cover"
-                fallback={<Box height="100%" />}
-            />
-        </Box>
-    );
+export default function ProfileCoverImage({
+  coverImage,
+  username,
+}: ProfileCoverImageProps) {
+  return (
+    <Box
+      position="relative"
+      w={{ base: "100vw", md: "100%" }}
+      maxW={{ base: "100vw", md: "container.lg" }}
+      mx={{ base: "unset", md: "auto" }}
+      overflow="hidden"
+      height={{ base: "120px", md: "200px" }}
+      p={0}
+      m={0}
+      mt={{ base: 0, md: 4 }}
+    >
+      <Image
+        src={coverImage}
+        alt={`${username} cover`}
+        w={{ base: "100vw", md: "100%" }}
+        h={{ base: "120px", md: "200px" }}
+        objectFit="cover"
+        fallback={<Box height="100%" />}
+      />
+    </Box>
+  );
 }
