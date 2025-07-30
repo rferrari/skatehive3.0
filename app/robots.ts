f7eaaed3dd666e23a33095ba208a15bd712a699c
sktbrd <sktbrd.eth@gmail.com>
2025-07-30 11:09:25 -0300

    feat: update robots configuration to specify crawl rules for different user agents and remove legacy robots.txt file

--- app/robots.ts
+++ app/robots.ts
@@ -13,10 +13,41 @@ export default function robots(): MetadataRoute.Robots {
                     '/settings/',
                     '/_next/',
                     '/pages/',
-                    '/src/'
+                    '/src/',
+                    '/test-thumbnails/',
+                    '/_vercel/',
+                    '/notifications.db'
                 ],
+                crawlDelay: 1,
+            },
+            {
+                userAgent: 'Googlebot',
+                allow: '/',
+                disallow: [
+                    '/admin/',
+                    '/api/',
+                    '/compose/',
+                    '/settings/',
+                    '/_next/',
+                    '/test-thumbnails/'
+                ],
+                crawlDelay: 0.5,
+            },
+            {
+                userAgent: 'Bingbot',
+                allow: '/',
+                disallow: [
+                    '/admin/',
+                    '/api/',
+                    '/compose/',
+                    '/settings/',
+                    '/_next/',
+                    '/test-thumbnails/'
+                ],
+                crawlDelay: 1,
             }
         ],
         sitemap: 'https://skatehive.app/sitemap.xml',
+        host: 'https://skatehive.app',
     };
 }
