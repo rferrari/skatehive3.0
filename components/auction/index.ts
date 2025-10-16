fbdb3ebe6528bf601670cda5e2e1ff3b00e30f00
sktbrd <sktbrd.eth@gmail.com>
2025-10-16 11:16:43 +0200

    refactor: Remove DualBalanceDemo component to streamline codebase

--- components/auction/index.ts
+++ components/auction/index.ts
@@ -1,7 +1,6 @@
 // Export all auction components from a single entry point
 export { default as AuctionCard } from './AuctionCard';
 export { AuctionBid } from './AuctionBid';
-export { AuctionWidget, QuickBidWidget } from './AuctionWidgets';
 export { BidsModal } from './BidsModal';
 export { default as AuctionPage } from './AuctionPage';
 export { AdminAuctionPage } from './AdminAuctionPage';
